581f9e010e055ac7b597cb61783c12df3359bb03
hit1808 <hiteshjain1808@gmail.com>
2015-04-22 20:38:05 -0400

    First Check in

---
+++ js/Data.js
@@ -0,0 +1,4 @@
+﻿var courses = [
+    { CourseNumber: "CS501", CourseName: "Intro to Java", Prereq: "CS400", CourseCoordinator: "Jurkat", TypiWC: "Fall" },
+    { CourseNumber: "CS502", CourseName: "Intro to C++", Prereq: "CS401", CourseCoordinator: "Pfeffer", TypiWC: "Spring" }
+];
